837e524f15ff7b5b3180ffb43cbc4ef9d83d8415
Marak <marak.squires@gmail.com>
2013-12-10 03:33:39 +0100

    [api] Add better error handling. Set validator to non-strict mode

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var rpc = {},
     mschema = require("mschema");
-
 var invoke = rpc.invoke = function (data, method, schema, callback) {
   // validate incoming input data based on schema
-  var validate = mschema.validate(data, schema.input);
+  var validate = mschema.validate(data, schema.input, { strict: false });
   if (!validate.valid) {
-    return callback(new Error('Validation error'), validate.errors);
+    return callback(new Error('Validation error: ' + JSON.stringify(validate.errors, true, 2)), validate.errors);
   }
   // execute remote method
-  method(data, function(err, result){
-    // validate outgoing output data based on schema
-    var validate = mschema.validate(result, schema.output);
+  method(data, function (err, result) {
+    // if the executed method has errored continue with error immediately
+    if (err) {
+      return callback(err);
+    }
+    // if no error was detected in executing the method attempt to,
+    // validate the method's output result based on schema
+    var validate = mschema.validate(result, schema.output, { strict: false });
     if (!validate.valid) {
       callback(validate.errors, result);
     } else {
