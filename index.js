b36505ab2d7df190988e1b3a7da81a700e0e4d4d
Marak <marak.squires@gmail.com>
2015-12-04 05:21:56 +0100

    [fix] Ensure that function scopes propagate

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var rpc = {},
     mschema = require("mschema");
 var invoke = rpc.invoke = function (data, method, schema, callback) {
+  var self = this;
   // validate incoming input data based on schema
   var validate = mschema.validate(data, schema.input, { strict: false });
   if (!validate.valid) {
     return callback(new Error('Validation error: ' + JSON.stringify(validate.errors, true, 2)), validate.errors);
   }
   // execute remote method
-  method(data, function (err, result) {
+  method.call(self, data, function (err, result) {
     // if the executed method has errored continue with error immediately
     if (err) {
       return callback(err);
@@ -16,9 +17,9 @@ var invoke = rpc.invoke = function (data, method, schema, callback) {
     // validate the method's output result based on schema
     var validate = mschema.validate(result, schema.output, { strict: false });
     if (!validate.valid) {
-      callback(validate.errors, result);
+      callback.call(self, validate.errors, result);
     } else {
-      callback(null, result);
+      callback.call(self, null, result);
     }
   });
 }
